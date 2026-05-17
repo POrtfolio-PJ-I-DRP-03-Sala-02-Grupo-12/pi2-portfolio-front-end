32c26ac8ce6878ebc96b8327b20a21d2a694b03b
junior8319 <tonhaun@yahoo.com.br>
2026-05-17 12:33:59 -0300

    feat-frontend-SSR: adaptation of vite configuration to SSR.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,33 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true, //Enables --host (already using it)
-    watch: {
-      usePolling: true, //Force polling (fixes Docker + WSL)
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     },
   },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
+  ssr: {
+    noExternal: ["@clerk/clerk-react"],
+  },
+
+  // server: {
+  //   host: true, //Enables --host (already using it)
+  //   watch: {
+  //     usePolling: true, //Force polling (fixes Docker + WSL)
+  //   },
+  // },
 });
