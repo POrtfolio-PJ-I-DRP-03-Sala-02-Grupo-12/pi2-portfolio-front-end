ef6a8fcb909b54f1fab93713978c241893f55756
junior8319 <tonhaun@yahoo.com.br>
2026-05-19 10:34:49 -0300

    feat-incorporation of metadata-incorporates metadata enhancings to server rendering.

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -3,12 +3,17 @@ import { StrictMode } from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router";
 import App from "./App.jsx";
+import { getSeoForPath } from "./config/routeSeo.js";
+import { buildHeadHtml } from "./utils/buildHeadHtml.js";
 import "./index.css";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 export function render(url) {
-  return renderToString(
+  const pathname = url.split("?")[0] || "/";
+  const head = buildHeadHtml(getSeoForPath(pathname));
+
+  const html = renderToString(
     <StrictMode>
       <ClerkProvider publishableKey={clerkPubKey}>
         <StaticRouter location={url}>
@@ -17,4 +22,6 @@ export function render(url) {
       </ClerkProvider>
     </StrictMode>
   );
+
+  return { html, head };
 }
